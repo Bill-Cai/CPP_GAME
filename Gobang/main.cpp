84783f90d23fb0f7eb0c49783be83042eb04a18f
Bill_Cai <qingmu_0722@163.com>
2024-09-28 16:13:42 +0800

    GUI

--- Gobang/main.cpp
+++ Gobang/main.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
+#include <graphics.h>
+#include <conio.h>
 #include "chess.hpp"
 
 int main()
 {
 
     Chess chess;
+    int blockSize = chess.getBlockSize();
+    initgraph(14 * blockSize, 14 * blockSize);
     while (1)
     {
         chess.printChessboard();
+        chess.drawChessboard();
         if (chess.check())
             break;
-        int playerFlag = chess.showPlayerFlag();
+        int playerFlag = chess.getPlayerFlag();
         if (playerFlag == 1)
         {
-            std::cout << "[BLACK]" << " Please enter the position of the next move: ";
-            int x, y;
-            std::cin >> x >> y;
-            chess.move(x, y);
+            while (1)
+            {
+                ExMessage msg = getmessage(EX_MOUSE);
+                if (msg.message == WM_LBUTTONDOWN)
+                {
+                    int x, y;
+                    x = chess.coord2mat(msg.x);
+                    y = chess.coord2mat(msg.y);
+                    chess.move(x, y);
+                    break;
+                }
+            }
         }
         else
         {
-            std::cout << "[WHITE]" << " Please enter the position of the next move: ";
-            int x, y;
-            std::cin >> x >> y;
-            chess.move(x, y);
+            while (1)
+            {
+                ExMessage msg = getmessage(EX_MOUSE);
+                if (msg.message == WM_LBUTTONDOWN)
+                {
+                    int x, y;
+                    x = chess.coord2mat(msg.x);
+                    y = chess.coord2mat(msg.y);
+                    chess.move(x, y);
+                    break;
+                }
+            }
         }
     }
+    _getch();
+    closegraph();
     return 0;
 }
